a84defe4ad695d7836446d0671e3d22ccf50894d
unknown <nirolph@697-334-Nogrod.sf.k12.internal>
2014-09-26 10:39:22 -0700

    rainbow fill.

--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SnowflakeCatcher extends PApplet {
+
 SnowFlake [] lotsAFlakes;
 int colorR = 1;
 int colorG = 1;
 int colorB = 1;
-void setup()
+public void setup()
 {
   size(500, 500);
   lotsAFlakes = new SnowFlake [300] ;
@@ -12,7 +28,7 @@ void setup()
  }
  background(0, 0, 0);
 }
-void draw()
+public void draw()
 {
   if (colorR >255) 
   {
@@ -41,7 +57,7 @@ void draw()
   }
 }
 
-void mouseDragged()
+public void mouseDragged()
 {
   if (mouseButton == LEFT) 
   {
@@ -64,14 +80,14 @@ class SnowFlake
     snowX = x;
     snowY = y;
   }
-  void show()
+  public void show()
   {
     stroke(255, 255,255);
     fill(255, 255, 255);
     ellipse(snowX, snowY, 10, 10);
    
   }
-  boolean lookDown()
+  public boolean lookDown()
   {
     if (get(snowX, snowY + 10) != color(0,0,0) && snowY > 0 && snowY< 480) 
     {
@@ -83,22 +99,31 @@ class SnowFlake
     }
   
   }
-  void erase()
+  public void erase()
   {
     fill(0, 0, 0);
     stroke(0, 0, 0);
     ellipse(snowX, snowY , 13, 13);
   }
-  void move()
+  public void move()
   {
   snowY = snowY +1;
   }
-  void wrap()
+  public void wrap()
   {
     if (snowY > 500) 
     {
       snowY = 0;
       snowX = (int)(Math.random()*500);   
+    }
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SnowflakeCatcher" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
     }
   }
 }
